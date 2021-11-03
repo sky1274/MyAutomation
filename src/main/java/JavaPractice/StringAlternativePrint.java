81453e8213e87ba1f28afb1528cf76a33d9d6852
Imran Shaik <campaigner.shaik@gmail.com>
2021-11-03 21:12:39 +0530

    My First commit

---
+++ src/main/java/JavaPractice/StringAlternativePrint.java
@@ -0,0 +1,25 @@
+package JavaPractice;
+
+public class StringAlternativePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String altString ="";
+		String name = "GeeksforGeeks";
+        int counter=1;
+        for(int i = 0; i<name.length(); i++){
+            
+            if(counter == 1){
+                altString =  altString + name.charAt(i);
+            }
+            if(counter > 1 && !(counter%2==0)){
+                altString =  altString + name.charAt(i);
+            }
+            counter++;
+        }
+        System.out.println(altString);
+
+	}
+
+}
